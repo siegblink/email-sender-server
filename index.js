e48b0546dda9f8be62536deac2ad009ea05e332d
Sieg <siegblink@gmail.com>
2020-03-27 01:00:59 +0800

    Setup passport with Google strategy.

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    function(accessToken) {
+      console.log(accessToken)
+    }
+  )
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, function() {
