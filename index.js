7178f39b139e72e5b528520cbed38d28e818a307
Sieg <siegblink@gmail.com>
2020-03-26 23:37:06 +0800

    Add passport and passport-google-oauth20 package.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send({ hi: 'there' })
-})
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, function() {
