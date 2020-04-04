32ae4819f93f67be1c2079fefccfb48477a1bc0f
Sieg <siegblink@gmail.com>
2020-04-05 07:29:20 +0800

    Refactor the project setup by adding routes and services directories.

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+require('dotenv').config()
+require('./services/passport')
 const app = express()
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    },
-    function(accessToken, refreshToken, profile, done) {
-      console.log('access token:', accessToken)
-      console.log('refresh token:', refreshToken)
-      console.log('profile:', profile)
-    }
-  )
-)
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, function() {
