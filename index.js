69dbdb3af908a12633a8c7e6b16b0363b7af820a
Sieg <siegblink@gmail.com>
2020-04-05 09:42:05 +0800

    Integrate mongoDB with mongoose library.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 require('./services/passport')
-const app = express()
 
+const app = express()
 require('./routes/authRoutes')(app)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, function() {
